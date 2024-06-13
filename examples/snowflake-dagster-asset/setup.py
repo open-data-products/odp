eb4b6027f3a3327efc719bb241396b039785ed9e
dexhorthy <dexter@metalytics.dev>
2024-06-12 20:41:10 -0500

    add scaffold asset

---
+++ examples/snowflake-dagster-asset/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="snowflake_dagster_asset",
+    packages=find_packages(exclude=["snowflake_dagster_asset_tests"]),
+    install_requires=[
+        "dagster",
+        "dagster-cloud"
+    ],
+    extras_require={"dev": ["dagster-webserver", "pytest"]},
+)
